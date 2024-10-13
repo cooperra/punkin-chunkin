2e0e18e33a735b1bcc6e1670e1f8dfa5d90fafb3
Robbie Cooper <cooperra@users.noreply.github.com>
2024-10-12 20:32:04 -0400

    Add launching ability

--- catapult_player.gd
+++ catapult_player.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+@export var projectile_scene: PackedScene = preload("res://pumpkin.tscn")
+@export var impulse = 25.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,3 +13,16 @@ func _physics_process(delta: float) -> void:
 	var turn_speed = 0.2
 	var rot_input = -Input.get_axis("ui_left", "ui_right")
 	rotate_y(rot_input * turn_speed * delta)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		_on_launch()
+
+
+func _on_launch():
+	var projectile: RigidBody3D = projectile_scene.instantiate()
+	projectile.position = $LaunchPoint.position
+	var root = get_tree().root
+	root.add_child(projectile)
+	projectile.apply_central_impulse($LaunchPoint.global_basis.z * impulse)
