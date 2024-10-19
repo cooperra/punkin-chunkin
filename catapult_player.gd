e7519543a79aa27d900a161c61d22b01d6464bad
--local <--local>
2024-10-19 19:22:08 -0400

    Launch with camera

--- catapult_player.gd
+++ catapult_player.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 @export var projectile_scene: PackedScene = preload("res://pumpkin.tscn")
-@export var impulse = 25.0
+@export var impulse_magnitude = 25.0
 @export var input_enabled: bool = true:
 	get(): return is_processing_unhandled_input()
 	set(value): set_process_unhandled_input(value)
@@ -28,12 +28,37 @@ func _unhandled_input(event: InputEvent) -> void:
 func _on_launch():
 	var projectile: RigidBody3D = projectile_scene.instantiate()
 	projectile.position = $LaunchPoint.position
-	var root = get_tree().root
+	var root: Node = get_tree().root
 	root.add_child(projectile)
-	projectile.apply_central_impulse($LaunchPoint.global_basis.z * impulse)
+	var impulse: Vector3 = $LaunchPoint.global_basis.z * impulse_magnitude
+	projectile.apply_central_impulse(impulse)
 
 	# Camera stuff here
 	# get current camera (attach script) (check if it's cool enough also)
 	# set target to projectile
 
 	# and I guess we'd need a way to get the camera to return home, but that's the whole point of making a second one.
+
+	# Ignore all that above
+	#deploy_chase_camera(projectile)
+	deploy_rb_camera(projectile, impulse)
+
+func deploy_chase_camera(target: Node3D) -> void:
+	var root = get_tree().root
+	var chase_camera = preload("res://chase_camera_3d.tscn").instantiate()
+	chase_camera.position = $Camera3D.global_position
+	chase_camera.basis = $Camera3D.global_basis
+	chase_camera.make_current()
+	chase_camera.target = target
+	root.add_child(chase_camera)
+
+func deploy_rb_camera(target: Node3D, impulse) -> void:
+	var root = get_tree().root
+	var rb_camera = preload("res://rigid_body_camera_3d.tscn").instantiate()
+	root.add_child(rb_camera)
+	rb_camera.transform = target.transform
+	rb_camera.target = target
+	rb_camera.camera.global_position = $Camera3D.global_position
+	rb_camera.camera.global_basis = $Camera3D.global_basis
+	rb_camera.camera.make_current()
+	rb_camera.delayed_launch(impulse, 0.2, 0.75)
