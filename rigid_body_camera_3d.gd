dee16e305a8c26c5a6bf1a5779a90bfab4421d1e
Robbie Cooper <cooperra@users.noreply.github.com>
2024-11-01 01:16:10 -0400

    Add wooshing sound to camera movement

--- rigid_body_camera_3d.gd
+++ rigid_body_camera_3d.gd
@@ -12,6 +12,9 @@ extends RigidBody3D
 #@export var current_velocity: Vector3 = Vector3.ZERO
 @onready var camera = %Camera3D
 
+var woosh_audio_tween: Tween
+var woosh_is_playing: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	set_process_unhandled_input(false)
@@ -19,9 +22,38 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	track_target()
+	update_woosh_audio()
+
+func track_target():
 	if not target:
 		return
 	camera.look_at(target.global_position)
+
+func update_woosh_audio():
+	var should_play = linear_velocity.length_squared() > 0
+	var woosh_is_playing = %WindSoundPlayer.playing
+	if not woosh_is_playing and should_play:
+		woosh_is_playing = true
+		# Ramp up volume
+		if woosh_audio_tween:
+			woosh_audio_tween.kill()
+		woosh_audio_tween = %WindSoundPlayer.create_tween()
+		woosh_audio_tween.tween_property(%WindSoundPlayer, "volume_db", 0, 0.2).from(-80)
+		# Random start
+		var stream: AudioStream = %WindSoundPlayer.stream
+		var start_point = randf() * stream.get_length()
+		# Play
+		%WindSoundPlayer.play(start_point)
+	if woosh_is_playing and not should_play:
+		woosh_is_playing = false
+		# Ramp down volume
+		if woosh_audio_tween:
+			woosh_audio_tween.kill()
+		woosh_audio_tween = %WindSoundPlayer.create_tween()
+		woosh_audio_tween.tween_property(%WindSoundPlayer, "volume_db", -80, 1).from_current()
+		# Stop after done
+		woosh_audio_tween.finished.connect(%WindSoundPlayer.stop)
 
 func delayed_launch(impulse: Vector3, launch_delay: float, camera_tween_duration: float):
 	apply_delayed_impulse(impulse, launch_delay)
