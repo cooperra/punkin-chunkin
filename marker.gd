3a5f997577f5d0fbe1a83c77784e7d10c4d6d6c0
Robbie Cooper <cooperra@users.noreply.github.com>
2024-11-01 03:52:32 -0400

    Convert marker distance text to yards

--- marker.gd
+++ marker.gd
@@ -4,7 +4,10 @@ var origin_position: Vector3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Label3D.text = "%.1d m" % global_position.distance_to(origin_position)
+	var meters = global_position.distance_to(origin_position)
+	var feet = 3.280839895 * meters
+	var yards = feet / 3
+	$Label3D.text = "%.1d yd" % yards
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
