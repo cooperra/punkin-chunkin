662c7102dee7cc7704bcec8702cf204e2d924844
Robbie Cooper <cooperra@users.noreply.github.com>
2024-11-01 01:26:55 -0400

    Despawn pumpkins 30 seconds after impact

--- projectile.gd
+++ projectile.gd
@@ -16,9 +16,15 @@ func _process(delta: float) -> void:
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	for contact_idx in range(state.get_contact_count()):
 		# We'll just call any collision good enough for now.
-		on_impact.emit();
+		_on_impact();
 		# TODO something elaborate
-		#var impulse: Vector3 = state.get_contact_impulse(contact_idx)
-		#if impulse.length() > 1:
-			#print("IMPULSE %s" % impulse.length())
-			#on_impact.emit()
+		var impulse: Vector3 = state.get_contact_impulse(contact_idx)
+		if impulse.length() > 1:
+			print("IMPULSE %s" % impulse.length())
+			print("  VELOCITY %s" % state.linear_velocity.length())
+			print("  CONTACT V %s" % state.get_contact_local_velocity_at_position(contact_idx))
+			#_on_impact()
+
+func _on_impact():
+	on_impact.emit()
+	get_tree().create_timer(30).timeout.connect(queue_free)
